87eb7e2526061d8a62389095c0830f900f1b23ee
Arab <dcskater20101784@aol.com>
2023-04-08 22:34:10 -0400

    updating

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from '../assets/Logo.svg';
+
+function Header() {
+  return (
+    <div><img src={Logo} alt='little lemon'/></div>
+  )
+}
+
+export default Header
---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+
+export default function Nav() {
+    return <nav className="nav">
+        <ul>
+            <li>
+                <a href='/Home'>Home</a>
+            </li>
+            <li>
+                <a href='About'>About</a>
+            </li>
+            <li>
+                <a href='Menu'>Menu</a>
+            </li>
+            <li>
+                <a href='Reservation'>Reservation</a>
+            </li>
+            <li>
+                <a href='Order Online'>Order Online</a>
+            </li>
+            <li>
+                <a href='Login'>Login</a>
+            </li>
+        </ul>
+    </nav>
+}
+
+
+
